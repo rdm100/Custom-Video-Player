d81861e37f4ba909d2e5658fbd084e4ac341764a
rdm100 <robertode_marco@hotmail.com>
2018-11-02 15:32:09 +0000

    Added progress, progress bar, toggle, skip buttons and ranges query selectors and wrote the toggle play function.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 // Get elements
-const player = document.querySelector(".player");
-const video = document.querySelector(".viewer");
-
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 // build functions
-
-
-
+function togglePlay(){
+	if(video.paused){
+		video.play();
+	} else {
+		video.pause();
+	}
+}
 // hook up the event listeners
